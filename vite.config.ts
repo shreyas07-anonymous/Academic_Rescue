56cc240d2d08260c93234969506a2190cea90e66
Shreyas Patankar <shreyaspatankar300@gmail.com>
2026-01-17 01:16:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
@@ -11,7 +10,6 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // Maintains your existing alias configuration
       '@': path.resolve(__dirname, '.'),
     },
   },
